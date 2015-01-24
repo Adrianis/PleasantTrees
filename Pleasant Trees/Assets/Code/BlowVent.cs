e9b6a38688a4f9af55d50ef9198cae6e8c637d1c
Adrianis <chester3000@hotmail.com>
2015-01-24 22:58:36 +0000

    Added code for Blow vents (propel you in a certain direction)

---
+++ Pleasant Trees/Assets/Code/BlowVent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlowVent : MonoBehaviour {
+
+    public float pushForce;
+
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            col.rigidbody.AddForce(transform.forward * pushForce, ForceMode.Force);
+        }
+    }
+
+}
