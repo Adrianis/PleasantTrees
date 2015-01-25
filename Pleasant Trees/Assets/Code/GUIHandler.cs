45aa2f395944a41e353982bc23267808b433b13e
Adrianis <chester3000@hotmail.com>
2015-01-25 11:18:55 +0000

    Added a gui handler to take o2 levels for both players & display in relevant screen areas

---
+++ Pleasant Trees/Assets/Code/GUIHandler.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIHandler : MonoBehaviour {
+
+    private Texture2D guiMeasureTex;
+    private GUIStyle guiMeasureStyle;
+    private Texture2D guiBackgroundTex;
+    private GUIStyle guiBackgroundStyle;
+
+    public float TimeOri;
+    public float Player1Time;
+    public float Player2Time;
+
+    void Start()
+    {
+        InitialiseGUIElements();
+    }
+
+    public void UpdatePlayerTime(int playerNum, float time)
+    {
+        if (playerNum == 1)
+        {
+            Player1Time = time;
+        }
+        else if (playerNum == 2)
+        {
+            Player2Time = time;
+        }
+    }
+
+    void OnGUI()
+    {
+        // Player 1
+        GUI.Label(new Rect(10, 30, 40, 20), "O2:");
+        GUI.Label(new Rect(50, 30, TimeOri * 7, 20), "", guiBackgroundStyle);
+        GUI.Label(new Rect(50, 30, Player1Time * 7, 20), "", guiMeasureStyle);
+
+        // Player 2
+        GUI.Label(new Rect(780, 30, 40, 20), "O2:");
+        GUI.Label(new Rect(820, 30, TimeOri * 7, 20), "", guiBackgroundStyle);
+        GUI.Label(new Rect(820, 30, Player2Time * 7, 20), "", guiMeasureStyle);
+    }
+
+    private void InitialiseGUIElements()
+    {
+        guiMeasureTex = new Texture2D(1, 1);
+        guiMeasureTex.SetPixel(1, 1, Color.white);
+        guiMeasureTex.wrapMode = TextureWrapMode.Repeat;
+        guiMeasureTex.Apply();
+        guiMeasureStyle = new GUIStyle();
+        guiMeasureStyle.normal.background = guiMeasureTex;
+
+
+        guiBackgroundTex = new Texture2D(1, 1);
+        guiBackgroundTex.SetPixel(1, 1, Color.gray);
+        guiBackgroundTex.wrapMode = TextureWrapMode.Repeat;
+        guiBackgroundTex.Apply();
+        guiBackgroundStyle = new GUIStyle();
+        guiBackgroundStyle.normal.background = guiBackgroundTex;
+    }
+
+}
